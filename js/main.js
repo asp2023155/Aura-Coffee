f30155c305fd72903e78331559e5bee4d96257bc
asp2023155 <asp2023155@as.rjt.ac.lk>
2026-03-13 15:45:53 +0530

    Add sticky navbar and back-to-top button functionality

---
+++ js/main.js
@@ -0,0 +1,33 @@
+(function ($) {
+    "use strict";
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 40) {
+            $('.navbar').addClass('sticky-top');
+        } else {
+            $('.navbar').removeClass('sticky-top');
+        }
+    });
+    
+
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+   
+    
+})(jQuery);
+
